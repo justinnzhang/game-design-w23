6ec7a849456e12dfd2df11cc574ecaa9d55508ab
Matthew Danics <matthewdanics@gmail.com>
2023-03-31 17:24:50 -0400

    confetti

--- src/components/CheckpointModal.tsx
+++ src/components/CheckpointModal.tsx
@@ -13,35 +13,76 @@ import {
   Image,
 } from '@chakra-ui/react';
 import { CheckpointProps } from './CheckpointDisplay';
+import { useReward } from 'react-rewards';
+import { useEffect } from 'react';
 
 interface Props {
   isOpen: boolean;
   onClose: () => void;
-  checkpoint: CheckpointProps;
+  checkpoint: CheckpointProps | null;
 }
 
 export const CheckpointModal = ({ isOpen, onClose, checkpoint }: Props) => {
-  if (checkpoint == null) return null;
+  const { reward, isAnimating } = useReward('confettiReward', 'confetti', {
+    lifetime: 500,
+    elementCount: 200,
+    spread: 75,
+    startVelocity: 50,
+  });
+  const { reward: balloonsReward, isAnimating: isBalloonsAnimating } =
+    useReward('balloonsReward', 'balloons', {
+      lifetime: 500,
+      elementCount: 50,
+      spread: 200,
+      startVelocity: 15,
+    });
+  const { reward: emojiReward, isAnimating: isEmojiAnimating } = useReward(
+    'emojiReward',
+    'emoji',
+    {
+      lifetime: 500,
+      elementCount: 50,
+      spread: 200,
+      startVelocity: 50,
+      emoji: ['☕️', '💰', '🤑', '💸'],
+    }
+  );
+
+  // call reward when modal opens
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(function () {
+        console.log('rewarding');
+        reward();
+        balloonsReward();
+        emojiReward();
+      }, 10);
+    }
+  }, [isOpen]);
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="lg">
+    <Modal isOpen={isOpen} onClose={onClose} size='lg'>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>
           🎉 Congratulations on making{' '}
-          {checkpoint.description.split(' ').slice(1).join(' ')} 🎉
+          {checkpoint?.description.split(' ').slice(1).join(' ')} 🎉
         </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Stack color="gray.600" spacing={4}>
+          <Stack color='gray.600' spacing={4}>
             <Image
-              src="/static/money-coffee.png"
-              alt="Picture of a coffee cup holding cash money"
+              src='/static/money-coffee.png'
+              alt='Picture of a coffee cup holding cash money'
             />
-            <Text>{checkpoint.completionText}</Text>
+            <Text>{checkpoint?.completionText}</Text>
           </Stack>
         </ModalBody>
         <ModalFooter>
-          <Button colorScheme="green" mr={3} onClick={onClose}>
+          <span id='confettiReward' />
+          <span id='balloonsReward' />
+          <span id='emojiReward' />
+          <Button colorScheme='green' mr={3} onClick={onClose}>
             Continue brewing
           </Button>
         </ModalFooter>
